a163b587d00768e4cb28cde602835d0706baeabd
KamilSobien <kamil.sobien@gmail.com>
2018-11-23 21:43:33 +0100

    FingerPrint class

--- app/src/main/java/com/example/kamil/bsm/FingerPrint.kt
+++ app/src/main/java/com/example/kamil/bsm/FingerPrint.kt
@@ -2,65 +2,61 @@ package com.example.kamil.bsm
 
 import android.Manifest
 import android.app.KeyguardManager
+import android.content.Context
 import android.content.pm.PackageManager
 import android.hardware.fingerprint.FingerprintManager
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.widget.Toast
-import java.security.KeyStore
-import android.security.keystore.KeyProperties
+import android.os.CancellationSignal
 import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyPermanentlyInvalidatedException
+import android.security.keystore.KeyProperties
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import java.io.IOException
-import java.security.InvalidAlgorithmParameterException
+import java.security.KeyStore
 import java.security.NoSuchAlgorithmException
 import java.security.NoSuchProviderException
-import java.security.cert.CertificateException
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
-import android.security.keystore.KeyPermanentlyInvalidatedException
 import javax.crypto.SecretKey
 
 
-class FingerPrintActivity : AppCompatActivity() {
-
+class FingerPrint(var context : Context, private var listener : FingerprintManager.AuthenticationCallback) {
     companion object {
         const val KEY_NAME = "com.example.kamil.bsm"
     }
-    private lateinit var fingerprintManager: FingerprintManager
     private lateinit var keyguardManager: KeyguardManager
+    private lateinit var fingerprintManager: FingerprintManager
     private lateinit var keyStore: KeyStore
     private lateinit var keyGenerator: KeyGenerator
     private lateinit var cipher: Cipher
     private lateinit var cryptoObject: FingerprintManager.CryptoObject
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_finger_print)
 
-        keyguardManager = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
-        fingerprintManager = getSystemService(FINGERPRINT_SERVICE) as FingerprintManager
+    fun init(){
+        keyguardManager = context.getSystemService(AppCompatActivity.KEYGUARD_SERVICE) as KeyguardManager
+        fingerprintManager = context.getSystemService(AppCompatActivity.FINGERPRINT_SERVICE) as FingerprintManager
 
         if (!keyguardManager.isKeyguardSecure) {
-            Toast.makeText(this, "Lock screen security not enabled in Settings", Toast.LENGTH_LONG).show()
+            Toast.makeText(context, "Lock screen security not enabled in Settings", Toast.LENGTH_LONG).show()
             return
         }
-
-        if (ActivityCompat.checkSelfPermission(this,  Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this,"Fingerprint authentication permission not enabled", Toast.LENGTH_LONG).show()
+        //Check whether the user has granted your app the USE_FINGERPRINT permission//
+        if (ActivityCompat.checkSelfPermission(context,  Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context,"Fingerprint authentication permission not enabled", Toast.LENGTH_LONG).show()
             return
         }
-
+        //Check that the user has registered at least one fingerprint//
         if (!fingerprintManager.hasEnrolledFingerprints()) {
-            Toast.makeText(this,    "Register at least one fingerprint in Settings", Toast.LENGTH_LONG).show()
+            Toast.makeText(context,    "Register at least one fingerprint in Settings", Toast.LENGTH_LONG).show()
             return
         }
         generateKey()
 
         if (cipherInit()) {
+            //If the cipher is initialized successfully, then create a CryptoObject instance//
             cryptoObject = FingerprintManager.CryptoObject(cipher)
-            val helper = FingerprintHandler(this)
-            helper.startAuth(fingerprintManager, cryptoObject)
+            fingerprintManager.authenticate(cryptoObject,CancellationSignal(),0,listener,null)
         }
     }
 
@@ -88,31 +84,20 @@ class FingerPrintActivity : AppCompatActivity() {
                     .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
                     .build())
             keyGenerator.generateKey()
-        } catch (e: NoSuchAlgorithmException) {
-            throw RuntimeException(e)
-        } catch (e: InvalidAlgorithmParameterException) {
-            throw RuntimeException(e)
-        } catch (e: CertificateException) {
-            throw RuntimeException(e)
         } catch (e: IOException) {
             throw RuntimeException(e)
         }
 
     }
-
-   private fun cipherInit():Boolean {
-        cipher = Cipher.getInstance( KeyProperties.KEY_ALGORITHM_AES + "/"
-                + KeyProperties.BLOCK_MODE_CBC + "/"
-                + KeyProperties.ENCRYPTION_PADDING_PKCS7)
-       return try {
-           keyStore.load(null)
-           val key = keyStore.getKey(KEY_NAME, null) as SecretKey
-           cipher.init(Cipher.ENCRYPT_MODE, key)
-           true
-       } catch (e : KeyPermanentlyInvalidatedException ){
-           false
-       }
+    private fun cipherInit():Boolean {
+        cipher = Cipher.getInstance( KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7)
+        return try {
+            keyStore.load(null)
+            val key = keyStore.getKey(KEY_NAME, null) as SecretKey
+            cipher.init(Cipher.ENCRYPT_MODE, key)
+            true
+        } catch (e : KeyPermanentlyInvalidatedException){
+            false
+        }
     }
 }
-
-
--- app/src/main/java/com/example/kamil/bsm/MainActivity.kt
+++ app/src/main/java/com/example/kamil/bsm/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kamil.bsm
 import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
+import android.hardware.fingerprint.FingerprintManager
 import android.os.Bundle
 import android.widget.Toast
 import com.example.kamil.bsm.algorithms.PBKDF2WithHmacSHA1
@@ -19,15 +20,32 @@ class MainActivity : Activity() {
         const val SharedPreferenceMessageVector = "MessageVector"
         val charset = StandardCharsets.UTF_8!!
     }
+
     private lateinit var prefs : SharedPreferences
 
+    private var fingerPrintListener = object : FingerprintManager.AuthenticationCallback(){
+        override fun onAuthenticationError(errMsgId: Int, errString: CharSequence) {
+            Toast.makeText(baseContext,"Authentication error\n$errString", Toast.LENGTH_LONG).show()
+        }
+        override fun onAuthenticationHelp(helpMsgId: Int, helpString: CharSequence) {
+            Toast.makeText(baseContext, "Authentication help\n$helpString", Toast.LENGTH_LONG).show()
+        }
+        override fun onAuthenticationFailed() {
+            Toast.makeText(baseContext,  "Authentication failed.", Toast.LENGTH_LONG).show()
+        }
+        override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult) {
+            Toast.makeText(baseContext, "Authentication succeeded.", Toast.LENGTH_LONG).show()
+            showMessage(InputPasswordPlainText.text.toString())
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        prefs =  getSharedPreferences(SharedPreferenceName,Context.MODE_PRIVATE)
+        prefs = getSharedPreferences(SharedPreferenceName,Context.MODE_PRIVATE)
 
-
+        // Enable fingerprint authorization
+        FingerPrint(baseContext,fingerPrintListener).init()
 
         ConfirmPasswordButton.setOnClickListener {
             val input = InputPasswordPlainText.text.toString()
@@ -36,8 +54,7 @@ class MainActivity : Activity() {
             clearViewContent()
 
             if (PBKDF2WithHmacSHA1.isCorrect(input, salt, key)){
-                // show Message
-                SecretMessageTextView.text = Message.getMessage(prefs, input)
+                showMessage(input)
             } else {
                 Toast.makeText(this,"Password is incorrect", Toast.LENGTH_LONG).show()
             }
@@ -66,6 +83,9 @@ class MainActivity : Activity() {
             }
             clearViewContent()
         }
+    }
+    fun showMessage(input : String){
+        SecretMessageTextView.text = Message.getMessage(prefs, input)
     }
     private fun clearViewContent(){
         ResetPasswordPlainText.text.clear()
